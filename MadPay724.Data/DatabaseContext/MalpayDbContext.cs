d20678c8fd9b1daa9aec19cc37f60c2b39b421e4
Ali Naghavi <58719284+M-Naghavi@users.noreply.github.com>
2020-06-09 10:44:36 +0430

    Se4-Ep1

---
+++ MadPay724.Data/DatabaseContext/MalpayDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MadPay724.Data.DatabaseContext
+{
+    public class MalpayDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.; Initial Catalog=MadPay724db; Integrated Security=true; MultipleActiveResultSets=true");
+        }
+    }
+}
