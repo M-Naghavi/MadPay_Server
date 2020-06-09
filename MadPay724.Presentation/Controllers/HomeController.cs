ae316e2d0cd59f5b908d1a4f8b3f56952e7cbb05
Ali Naghavi <58719284+M-Naghavi@users.noreply.github.com>
2020-06-09 09:51:57 +0430

    init project

---
+++ MadPay724.Presentation/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MadPay724.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get()
+        {
+            return new string[] { "1", "2" };
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public async Task<string> Post([FromBody] string value)
+        {
+            return null;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<string> Put(int id,[FromBody] string value)
+        {
+            return null;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<string> Delete(int id)
+        {
+            return null;
+        }
+    }
+}
